a4738f8ad2895578baca0f9dc9ceea32b15faf5e
test <golf080174@gmail.com>
2018-04-25 15:06:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.myapplication.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+    } //Main Method
+
+}   //Main Class
